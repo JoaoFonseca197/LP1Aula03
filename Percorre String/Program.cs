38f36445c717a6d4ce4c05ca98b4565e62b3b1a9
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-03-26 10:10:58 +0000

    Second Commit

---
+++ Percorre String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Percorre_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            string str,cInString;
+            char c;
+            //pedir ao utilizador string
+            Console.Write("Insira a string:");
+            str = Console.ReadLine();
+            //pedir ao utilizador caracter
+            Console.Write("Insira a caracter:");
+            cInString = Console.ReadLine();
+            c = Convert.ToChar(cInString)
+            //percorrer a string
+            foreach( char cInString in str);
+                if(cInString != c)
+                {
+                    Console.Write(cDastr);
+                }
+            // imprimir string sem caracter
+        }
+    }
+}
