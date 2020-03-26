37ca5759291ecf5a73571316b777577601a7d754
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-03-26 10:53:32 +0000

    Thierd commit

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -6,7 +6,45 @@ namespace ArrayBiDim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //Declarar variaveid
+           string str;
+           int linhas, colunas;
+           float [,] matriz;
+           float somaDasMedias = 0;
+
+           //Pedir ao useer nº linhas da matriz
+           Console.Write("Insira nº de linhas:");
+           str = Console.ReadLine();
+           colunas = int.Parse(str);
+           //inicializar a matriz
+           matriz = new float [linhas, colunas];
+
+           //Pedir ao user os valores float para colocar na matriz
+           for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"Insira valor em ({i}{j}):");
+                    str = Console.ReadLine();
+                    matriz[i, j] = Convert.ToSingle(str);
+                }
+            }
+            //mostrar matriz e a media para cada linha
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                float soma;
+                Console.Write("| ");
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    soma += 0;
+                    Console.Write(matriz[i,j]+" ");
+                }
+                media = soma / matriz.GetLength(1);
+                Console.WriteLine($"| -> {media}");
+                somaDasMedias += media;
+                //Mostrar soma das medias 
+                Console.WriteLine("Soma das medias {0,10:f3}",somaDasMedias);
+            }
         }
     }
 }
